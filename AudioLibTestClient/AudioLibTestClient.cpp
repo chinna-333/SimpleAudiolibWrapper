6d8ece9ba1b0115e4644a68583203c807b784e4f
chinna samudrudu <c.samudrudu333@gmail.com>
2018-11-24 19:36:26 +0530

    added comments and updated example in client main

--- AudioLibTestClient/AudioLibTestClient.cpp
+++ AudioLibTestClient/AudioLibTestClient.cpp
@@ -1,17 +1,26 @@
 #include "stdafx.h"
 #include "audiolib.h"
 #include <Windows.h>
+#include <stdio.h>
 
-// sound assets are placed under "AudioLib/sound assets"
+// sound assets are placed under "AudioLib/sound assets" and defined in audiolib.h
+
 // currently three sound assets are defined in dll (audiolib.h) -- SOUND_ONE, SOUND_TWO, SOUND_THREE
 // To add different sounds place the new sounds in above mentioned folder and define in audiolib.h
+
 // you can use playSound function to play sound files not in assets also. 
 // simply pass the path (remember to escape every "\") to the sound file in the funtion argument.
+
 // note that only .wav files are currently supported.
+
+// always terminate the program with exit(0) if addSoundToQueue() or playSoundLoop() is used. 
+// because they are async methods and are not terminated implicitly by underlying windows api.
 
 int main(){
 	playSoundLoop(SOUND_ONE);
 	Sleep(5000);
-	playSound(SOUND_TWO);
-	return 0;
+	playSound(SOUND_THREE);
+	printf("test\n");
+	system("pause");
+	exit(0);
 }
